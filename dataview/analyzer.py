2a6dc67fcc78cc4af65d93458ccfc0e69c1fcda5
niklasblock <niklas.block.business@gmail.com>
2026-05-10 08:18:03 +0200

    add analyzer with largest files, folders, file types and old files checks

--- dataview/analyzer.py
+++ dataview/analyzer.py
@@ -1,43 +1,62 @@
 #dataview/analyzer.py 
 
-from pathlib import Path
+from datetime import datetime, timedelta
+from collections import defaultdict
 
 class Analyzer: 
 
-    def __init__(self, path: Path) -> None: 
+    def __init__(self, files: list[dict]) -> None: #bekommt Scanner-Output
         """Initialize Analyzer with the path from the drive.
     
         Args:
             path: Path to the drive to analyze
         """
-        self.path = path 
+        self.files = files 
 
-    def analyze(self) -> list[str]:
+    def analyze(self) -> dict:
         """Analyse Path File 
     
         Returns:
             List: Warning str of missing docstrings
         """
 
-        result = []
+        return {
+            "largest_files": self._top_largest_files(),
+            "largest_folders": self._top_largest_folders(),
+            "file_types": self._file_types(),
+            "old_files": self._old_files()
+        }
+    
+    def _top_largest_files(self, n: int = 10) -> list[dict]: 
+        """Return top 10 largest files"""
+        # Dateien nach Größe sortieren
+        sorted_files = sorted(self.files, key=lambda f: f["size"], reverse=True)
 
-        #später dann alle warnung wie auch type hints 
-        return result
+        return sorted_files[:n]
 
-    def _check_size_folder(self, tree): 
-        pass
+    def _top_largest_folders(self, n: int = 10) -> list[dict]: 
+        """Return all folder sizes of the input drive"""
+        # Ordnergrößen berechnen
+        
+        folder_sizes = defaultdict(int)
+        for f in self.files:
+            folder_sizes[f["path"].parent] += f["size"]
 
-    def _check_size_files(self): 
-        pass 
+        # sortieren und als Liste zurückgeben
+        sorted_folders = sorted(folder_sizes.items(), key=lambda x: x[1], reverse=True)
+        return [{"path": k, "size": v} for k, v in sorted_folders[:n]]
 
-    def _check_double_data(self):
-        """folder, files the heaving the same name"""
-        pass 
+    def _file_types(self) -> dict[str, int]:           # {"pdf": 42, "csv": 13}
+        """Return all files types and the amout"""
+        file_types = defaultdict(int)
+        for file in self.files:
+            file_types[file["suffix"]] += 1
+        return dict(file_types)
 
-    def _check_data_older_than_x_years(self): 
-        pass 
+    def _old_files(self, years: int = 2) -> list[dict]: 
+        """Return all files witch are older than 2 years"""
+        # Alte Dateien filtern
+        cutoff = datetime.now() - timedelta(days=years * 365)
+        old = [f for f in self.files if f["modified"] < cutoff]
 
-    #! ich möchte dann auch so gruppen habe, 
-    # pdf, docx, excel ... -> informationen 
-    # png, icon, ... -> video, bild 
-    # ... irgendwie so 
+        return old 
